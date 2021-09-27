e51021c7278ba3b4186fef1627d76af8f9eebc7c
Prithviraj2511 <prithvirajpatil2511@gmail.com>
2021-09-27 12:01:48 +0530

    Revert "added Location listing screen"
    This reverts commit ffc465a546b5a182b88a3a3b01f97a4af0423e08.

--- app/src/main/java/com/example/touristguide/HomeActivity.java
+++ app/src/main/java/com/example/touristguide/HomeActivity.java
@@ -2,10 +2,7 @@ package com.example.touristguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,17 +10,5 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        ImageView img = (ImageView) findViewById(R.id.topPicksImg);
-        img.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // your code here
-                getTopPicks();
-            }
-        });
-    }
-
-    private void getTopPicks(){
-        Intent intent = new Intent(this, LocationListing.class);
-        startActivity(intent);
     }
 }
