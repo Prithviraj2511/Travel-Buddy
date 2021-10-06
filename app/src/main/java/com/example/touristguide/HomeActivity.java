3afcd443a423ed5794e8a9489737a0ca04717ab4
pranavdot <59735038+pranavdot@users.noreply.github.com>
2021-10-07 03:16:00 +0530

    current_location

--- app/src/main/java/com/example/touristguide/HomeActivity.java
+++ app/src/main/java/com/example/touristguide/HomeActivity.java
@@ -5,20 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         ImageView topPicksImg = (ImageView) findViewById(R.id.topPicksImg);
         topPicksImg.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // your code here
                 System.out.println("Clicked");
-                topPicksImg.animate().rotationY(topPicksImg.getRotationY()+360).setDuration(300).withEndAction(new Runnable() {
+                topPicksImg.animate().rotationY(topPicksImg.getRotationY() + 360).setDuration(300).withEndAction(new Runnable() {
                     @Override
                     public void run() {
                         topPicksImg.animate().alpha(1);
@@ -26,11 +29,28 @@ public class HomeActivity extends AppCompatActivity {
                     }
                 });
             }
+
+        });
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+
+
         });
     }
+
+
 
     private void getTopPicks(){
         Intent intent = new Intent(this, LocationListing.class);
         startActivity(intent);
     }
+    private void openActivity2() {
+        Intent intent = new Intent(this,currentloc.class);
+        startActivity(intent);
+    }
 }
+
---
+++ app/src/main/java/com/example/touristguide/currentloc.java
@@ -0,0 +1,96 @@
+package com.example.touristguide;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class currentloc extends AppCompatActivity {
+
+    //Initialize variable
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currentloc);
+
+
+        //Assign variable
+        supportMapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+
+        //Initialize fused location
+        client= LocationServices.getFusedLocationProviderClient(this);
+
+        if(ActivityCompat.checkSelfPermission(currentloc.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //When Permission granted
+            //Call method
+            getCurrentLocation();
+
+        }else {
+            //When permission denied
+            //Request permission
+            ActivityCompat.requestPermissions(currentloc.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+        }
+
+    }
+
+    private void getCurrentLocation() {
+        //Initialize task location
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                //When success
+                if(location != null){
+                    //Sync map
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+                            //Initialize lat lng
+                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                            //Create marker options
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("I am here");
+                            //Zoom map
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                            //Add marker on map
+                            googleMap.addMarker(options);
+
+
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 44){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //When permission granted
+                //Call method
+                getCurrentLocation();
+            }
+        }
+    }
+}
+
+
